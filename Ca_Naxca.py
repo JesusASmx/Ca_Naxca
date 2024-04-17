5c36b76bc9776fed3a35c62be03d0d9a26ede09c
Jesús Armenta-Segura <138358307+JesusASmx@users.noreply.github.com>
2024-04-17 00:35:38 -0600

    Add files via upload

---
+++ Ca_Naxca.py
@@ -0,0 +1,14 @@
+class csv_crossvals:
+    def __init__(self):
+        print("WORKS!")
+        return
+
+
+#class csv_crossvals:
+#    def __init__(self):
+
+#class csv_dataset:
+
+#class collator_regression:
+
+#class regression_report:
